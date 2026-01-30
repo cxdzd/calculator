e42cc3a59d2e6f170b1436512483e559315e4c97
unknown <astono.jordan@gmail.com>
2026-01-30 15:18:28 +1100

    basic calculator made

---
+++ basicCalculator.py
@@ -0,0 +1,25 @@
+while True:
+    num1 = input("First number: ")
+    num2 = input("Second number")
+    
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    op = input(int("Which operation (1/2/3/4): "))
+
+    if op == 1:
+        print(num1 + num2)
+        break
+    elif op == 2:
+        print(num1 - num2)
+        break
+    elif op == 3:
+        print(num1 * num2)
+        break
+    elif op == 4:
+        print(num1 / num2)
+        break
+    else:
+        print("Invalid, try agian")
